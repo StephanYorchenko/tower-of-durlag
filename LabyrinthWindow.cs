c0dad33418fb2990ce4c7fc44f36ae7052087ceb
Aidar Shaikhullin <Shaikhullin.aidar@gmail.com>
2020-05-02 14:05:37 +0500

    Add visualization

--- LabyrinthWindow.cs
+++ LabyrinthWindow.cs
@@ -1,14 +1,41 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 
 namespace LabirintDemoGame
 {
     public class LabyrinthWindow : Form
     {
-        public LabyrinthWindow(Game labyrinth)
+        private readonly Dictionary<string, Bitmap> bitmaps = new Dictionary<string, Bitmap>();
+        private readonly Game game;
+        
+        public LabyrinthWindow(Game labyrinth, DirectoryInfo imagesDirectory = null)
         {
-            ClientSize = new Size(32 * labyrinth.MazeWidth, 32 * labyrinth.MazeHeight);
-            FormBorderStyle = FormBorderStyle.FixedDialog; // потом можно будет поставить None и сделать свой креатик
+            game = labyrinth;
+            ClientSize = new Size(32 * labyrinth.MazeHeight, 32 * labyrinth.MazeWidth);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            if (imagesDirectory == null)
+                imagesDirectory = new DirectoryInfo("Images");
+            foreach (var e in imagesDirectory.GetFiles("*.png"))
+                bitmaps[e.Name.Substring(0, e.Name.Length - 4)] = (Bitmap) Image.FromFile(e.FullName);
         }
+        
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Black, 0, 0, 32 * game.MazeHeight, 32 * game.MazeWidth);
+            for (int i = 0; i < game.MazeHeight; i++)
+            {
+                for (int j = 0; j < game.MazeWidth; j++)
+                {
+                    if (game.Maze[i, j].Type.ToString() != "Empty")
+                    {
+                        e.Graphics.DrawImage(bitmaps[game.Maze[i, j].Type.ToString()], new Point(i * 32, j * 32));
+                    }
+                }
+            }
+            e.Graphics.ResetTransform();
+        }
+        
     }
 }
