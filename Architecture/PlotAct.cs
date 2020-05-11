02b71c90b73b378ce47384a8f7e010ef8f0c22d9
Stepan Yurchenko <stepan33314@yandex.ru>
2020-05-11 21:21:11 +0500

    remove using

--- Architecture/PlotAct.cs
+++ Architecture/PlotAct.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.IO;
-using System.Linq.Expressions;
 using Newtonsoft.Json;
 
 namespace LabirintDemoGame.Architecture
