e81271b6aed6cbe0c0471a8a5cc2ca217cba5405
Stepan Yurchenko <stepan33314@yandex.ru>
2020-05-11 21:24:08 +0500

    test push

--- Architecture/Options.cs
+++ Architecture/Options.cs
@@ -4,16 +4,6 @@ namespace LabirintDemoGame.Architecture
 {
     public class Option : PlotAction
     {
-        public string Name;
-        public int Torch;
-        public int Bandage;
-        public int Herb;
-        public bool Sword;
-        public int Gold;
-        public int Supplies;
-        public int HP;
-        public string Result;
-
         public bool IsValid(Player player)
         {
             return true;
